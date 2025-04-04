9408357f6a81a7cec49b071d6c7adc28bf16b53b
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-04-03 20:51:49 -0500

    feat: adding model

--- src/main/java/com/tgcannabis/model/SensorData.java
+++ src/main/java/com/tgcannabis/model/SensorData.java
@@ -1,4 +1,14 @@
 package com.tgcannabis.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SensorData {
+    private SensorInformation sensorName;
+    private double value;
+    private long timestamp;
 }
--- src/main/java/com/tgcannabis/model/SensorInformation.java
+++ src/main/java/com/tgcannabis/model/SensorInformation.java
@@ -1,4 +1,14 @@
 package com.tgcannabis.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SensorInformation {
+    String sensorType;
+    String location;
+    String id;
 }
